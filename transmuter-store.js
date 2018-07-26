27451f0c4cb6432057641d803e2cd66e6880e5dd
Iain van der Wiel <iain.vanderwiel@gmail.com>
2018-07-26 22:33:06 +0200

    Fixed passing state to handler

--- transmuter-store.js
+++ transmuter-store.js
@@ -24,7 +24,7 @@ class TransmuterStore {
             prop,
             value,
             oldValue: store[prop],
-            state: store.state,
+            state: this.state,
           },
         });
 
